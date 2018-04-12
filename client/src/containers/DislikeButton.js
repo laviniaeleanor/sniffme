9560de51ecebbbbb1cf63ab00fb1b36eeb20f557
LisaScorzon <lisabailey.eu@gmail.com>
2018-04-12 14:25:30 +0200

    Like and DislikeButton

--- client/src/containers/DislikeButton.js
+++ client/src/containers/DislikeButton.js
@@ -2,12 +2,60 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-const mapStateToProps = (reduxState) => {
 
-    return {
-    component: reduxState.component
-    }
+
+// import PropTypes from "prop-types"
+// import { connect } from "react-redux"
+// import {newPicture} from '../actions/actions'
+
+export class DislikeButton extends PureComponent {
+
+    newPicture() {
+    this.props.newPicture()
+  }
+
+  //
+  // onclickhandler = (e) => {
+  //     handleSubmit(e)
+  //     this.props.newPicture()
+  // }
+     // handleSubmit = (e) => {  // prevents the page from re-loading
+     //         // e.preventDefault()
+     //         this.props.newPicture()
+     //       }
+
+
+     render() {
+       return (
+           //<div>
+               <button className= "DislikeButton" onClick={(e) => {e.preventDefault(); this.newPicture()}}>DislikeButton </button>
+
+           //</div>
+           )
+       }
 }
 
 
-export default connect(mapStateToProps)(Component)
+
+// function mapStateToProps(state) {  // mapping state from  store to props?
+//
+//    return {
+//    LikeButton: state.LikeButton
+//    }
+// }
+
+
+// export default connect(mapStateToProps)(LikeButton) // mapping to component
+
+
+// constructor(props){
+//   super(props)
+//   this.onclickhandler = this.onclickhandler.bind(this)
+//     }
+
+//const mapStateToProps = (reduxState) => {
+
+  //  return {
+    //component: reduxState.component
+    //}
+//}
--- client/src/containers/LikeButton.js
+++ client/src/containers/LikeButton.js
@@ -1,13 +1,49 @@
 import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+// import PropTypes from "prop-types"
+// import { connect } from "react-redux"
+// import {newPicture} from '../actions/actions'
 
-const mapStateToProps = (reduxState) => {
+export class LikeButton extends PureComponent {
 
-    return {
-    component: reduxState.component
-    }
+    newPicture() {
+    this.props.newPicture()
+  }
+
+  //
+  // onclickhandler = (e) => {
+  //     handleSubmit(e)
+  //     this.props.newPicture()
+  // }
+     // handleSubmit = (e) => {  // prevents the page from re-loading
+     //         // e.preventDefault()
+     //         this.props.newPicture()
+     //       }
+
+
+     render() {
+       return (
+           //<div>
+               <button className= "LikeButton" onClick={(e) => {e.preventDefault(); this.newPicture()}}>LikeButton </button>
+
+           //</div>
+           )
+       }
 }
 
 
-export default connect(mapStateToProps)(Component)
+
+// function mapStateToProps(state) {  // mapping state from  store to props?
+//
+//    return {
+//    LikeButton: state.LikeButton
+//    }
+// }
+
+
+// export default connect(mapStateToProps)(LikeButton) // mapping to component
+
+
+// constructor(props){
+//   super(props)
+//   this.onclickhandler = this.onclickhandler.bind(this)
+//     }
