86534dac9c0857457b8e543c4438bbed41c3e8de
LisaScorzon <lisabailey.eu@gmail.com>
2018-04-12 15:22:31 +0200

    dislike button update

--- client/src/containers/DislikeButton.js
+++ client/src/containers/DislikeButton.js
@@ -4,58 +4,18 @@ import { connect } from 'react-redux'
 
 
 
-// import PropTypes from "prop-types"
-// import { connect } from "react-redux"
-// import {newPicture} from '../actions/actions'
-
-export class DislikeButton extends PureComponent {
-
-    newPicture() {
-    this.props.newPicture()
+class DislikeButton extends PureComponent {
+  randomImage() {
+    console.log('test')
   }
 
-  //
-  // onclickhandler = (e) => {
-  //     handleSubmit(e)
-  //     this.props.newPicture()
-  // }
-     // handleSubmit = (e) => {  // prevents the page from re-loading
-     //         // e.preventDefault()
-     //         this.props.newPicture()
-     //       }
-
-
-     render() {
-       return (
-           //<div>
-               <button className= "DislikeButton" onClick={(e) => {e.preventDefault(); this.newPicture()}}>DislikeButton </button>
-
-           //</div>
-           )
-       }
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={() => this.randomImage()}>STINKY!</button>
+      </div>
+    );
+  }
 }
 
-
-
-// function mapStateToProps(state) {  // mapping state from  store to props?
-//
-//    return {
-//    LikeButton: state.LikeButton
-//    }
-// }
-
-
-// export default connect(mapStateToProps)(LikeButton) // mapping to component
-
-
-// constructor(props){
-//   super(props)
-//   this.onclickhandler = this.onclickhandler.bind(this)
-//     }
-
-//const mapStateToProps = (reduxState) => {
-
-  //  return {
-    //component: reduxState.component
-    //}
-//}
+export default DislikeButton;
