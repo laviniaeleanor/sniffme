1f7db7455eee24033ec67c5aaddc01343bfc22fe
laviniaeleanor <36698431+laviniaeleanor@users.noreply.github.com>
2018-04-12 16:15:28 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Page from './components/Page'
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
-import {LikeButton} from './containers/LikeButton'
-import {DislikeButton} from './containers/DislikeButton'
 
 
 
@@ -15,8 +13,6 @@ class App extends Component {
             <div>
                 <Route exact path="/sniffme" component={Page} />
                 <Route exact path="/" render={ () => <Redirect to="/sniffme" /> } />
-            <LikeButton/>
-            <DislikeButton/>
             </div>
         </Router>
 )
