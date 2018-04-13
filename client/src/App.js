40ac2096dbbac96e7930b27f2b4dc1eccd215526
lolita89 <cika.lolita@hotmail.com>
2018-04-13 12:10:25 +0200

    the matched page shown

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Page from './components/Page'
+import MatchPage from './components/MatchPage'
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
@@ -13,6 +14,7 @@ class App extends Component {
             <div>
                 <Route exact path="/sniffme" component={Page} />
                 <Route exact path="/" render={ () => <Redirect to="/sniffme" /> } />
+                <Route exact path="/sniffme/matchuser" component={MatchPage} />
             </div>
         </Router>
 )
