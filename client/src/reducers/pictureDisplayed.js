d5072c8947951963291b1e76e653ea5893ca2e87
Irene de Nicolo <irene.denicolo89@gmail.com>
2018-04-13 10:13:59 +0200

    loading image

--- client/src/reducers/pictureDisplayed.js
+++ client/src/reducers/pictureDisplayed.js
@@ -1,6 +1,6 @@
 import { NEW_PICTURE } from '../actions/actions'
 
-const initialState = "https://www.what-dog.net/Images/faces2/main008.jpg"
+const initialState = "https://www.stonewallproductions.co.nz/images/425444/progress_bar_dog.gif"
 
 export default (state = initialState, action) => {
   switch (action.type) {
